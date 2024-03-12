31bc4a04f01b13944f91f7144a37029a39c8ee58
annaSemenova <anna.7nova@yandex.by>
2024-03-13 01:13:00 +0300

    add link between pages and fix content

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,20 +1,34 @@
 import Image from "next/image";
-import styles from ".././page.module.css";
+import styles from "../page.module.css";
+import Link from 'next/link'
 
 export default function Blog() {
   return (
     <main className={styles.main}>
+
       <div className={styles.description}>
-        <p>
-          This is our blog about next.js course
-        </p>
-        <p>
-          Here you can send your questions
-        </p>
-        <p>
-          Here is support
-        </p>
+        <h2>
+          <Link className={styles.card} href="../">Home</Link>
+        </h2>
+      </div>
+
+      <div className={styles.description} >
+        <div>
+          <h3>
+            This is our blog about next.js course
+          </h3>
+          <p>
+            Layouts
+            A layout is UI that is shared between multiple routes. On navigation, layouts preserve state, remain interactive, and do not re-render. Layouts can also be nested.
+            You can define a layout by default exporting a React component from a layout.js file. The component should accept a children prop that will be populated with a child layout (if it exists) or a page during rendering.
+          </p>
         </div>
+
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,15 +40,14 @@ export default function Home() {
 
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/blog"
           className={styles.card}
-          target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            Blog <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>Here is the up-to-date information about our course.</p>
         </a>
 
         <a
